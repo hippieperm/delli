cfaea7c6e2ae277ea7fcb687eff020f7f96a4e79
Jutole <rlatkd0727@gmail.com>
2023-06-21 22:37:14 +0900

    공통 위젯 설정

---
+++ lib/common_widgets/common_layout.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class CommonLayout extends StatelessWidget {
+  const CommonLayout({
+    required this.child,
+    Key? key,
+  }) : super(key: key);
+
+  final Widget child;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 30),
+        child: child,
+      ),
+    );
+  }
+}
