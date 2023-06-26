cde99c5866501cfc86173c47f746418c10d314dd
xoxo <ghl0617@gmail.com>
2023-06-27 01:21:08 +0900

    공통버튼 제작

---
+++ lib/common_widgets/common_text_button.dart
@@ -0,0 +1,49 @@
+import 'package:delli/common_widgets/common_text.dart';
+import 'package:delli/styles/delli_color.dart';
+import 'package:flutter/material.dart';
+
+class CommonTextButton extends StatelessWidget {
+  const CommonTextButton({
+    required this.onPressed,
+    required this.commonText,
+    this.height,
+    this.width,
+    this.backgroundColor = DelliColor.mainColor,
+    this.enabled,
+    this.borderRadius = 10,
+    super.key,
+  });
+
+  final double? width;
+  final double? height;
+  final double borderRadius;
+  final Color backgroundColor;
+
+  final CommonText commonText;
+
+  final bool? enabled;
+  final Function()? onPressed;
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      width: width ?? double.infinity,
+      height: height,
+      child: ElevatedButton(
+        onPressed: enabled == null
+            ? onPressed
+            : enabled == true
+                ? onPressed
+                : null,
+        style: ElevatedButton.styleFrom(
+          backgroundColor: backgroundColor,
+          disabledBackgroundColor: backgroundColor,
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(borderRadius),
+          ),
+        ),
+        child: commonText,
+      ),
+    );
+  }
+}
