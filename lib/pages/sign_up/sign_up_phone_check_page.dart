5e68a338913e7cefcfa2582ccfb9e3deffea3ae1
hippieperm <xoxockstar@gmail.com>
2023-07-10 13:01:01 +0900

    폰번호 인증 페이지 수정

--- lib/pages/sign_up/sign_up_phone_check_page.dart
+++ lib/pages/sign_up/sign_up_phone_check_page.dart
@@ -2,6 +2,7 @@ import 'package:delli/common_widgets/common_layout.dart';
 import 'package:delli/common_widgets/common_outline_text_form_field.dart';
 import 'package:delli/common_widgets/common_text.dart';
 import 'package:delli/common_widgets/common_text_button.dart';
+import 'package:delli/styles/delli_color.dart';
 import 'package:delli/styles/delli_text_styles.dart';
 import 'package:flutter/material.dart';
 
@@ -18,19 +19,37 @@ class SignUpPhoneCheckPage extends StatelessWidget {
           const CommonText(
             text: '델리에서 회원가입을 위해\n휴대전화 번호를 인증해 주세요',
             textStyle: DelliTextStyles.header3,
+            color: DelliColor.black,
           ),
           const SizedBox(height: 9),
           const CommonText(
             text: '휴대전화 번호는 본인 인증을 위해서만 사용됩니다.',
             textStyle: DelliTextStyles.body2,
+            color: DelliColor.gray88,
           ),
           const SizedBox(height: 37),
-          const _row(),
+          Row(
+            children: [
+              const Expanded(
+                child: CommonOutlineTextFormField(
+                  hintText: '휴대전화 번호를 입력해 주세요.',
+                ),
+              ),
+              const SizedBox(width: 12),
+              CommonTextButton(
+                onPressed: () {},
+                commonText: const CommonText(
+                  text: '인증 요청',
+                  textStyle: DelliTextStyles.body1_500,
+                ),
+                width: 92,
+                height: 50,
+              ),
+            ],
+          ),
           const SizedBox(height: 27),
-          const Expanded(
-            child: CommonOutlineTextFormField(
-              hintText: '인증번호를 입력해주세요.',
-            ),
+          const CommonOutlineTextFormField(
+            hintText: '인증번호를 입력해 주세요.',
           ),
           const Spacer(),
           CommonTextButton(
@@ -44,33 +63,6 @@ class SignUpPhoneCheckPage extends StatelessWidget {
           const SizedBox(height: 55),
         ],
       ),
-    );
-  }
-}
-
-class _row extends StatelessWidget {
-  const _row({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Row(
-      children: [
-        const Expanded(
-          child: CommonOutlineTextFormField(
-            hintText: '휴대전화 번호를 입력해주세요.',
-          ),
-        ),
-        const SizedBox(width: 12),
-        CommonTextButton(
-          onPressed: () {},
-          commonText: const CommonText(
-            text: '인증 요청',
-            textStyle: DelliTextStyles.body1_500,
-          ),
-          width: 92,
-          height: 50,
-        ),
-      ],
     );
   }
 }
