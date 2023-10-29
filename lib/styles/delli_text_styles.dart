0b4931eaab87653970cda8498f36111ef0fe6159
hippieperm <xoxockstar@gmail.com>
2023-10-29 23:15:07 +0900

    폰트 추가

--- lib/styles/delli_text_styles.dart
+++ lib/styles/delli_text_styles.dart
@@ -22,6 +22,13 @@ class DelliTextStyles {
     height: 1.5,
   );
 
+  static const TextStyle body1_600 = TextStyle(
+    fontFamily: "Pretendard",
+    fontSize: 16,
+    fontWeight: FontWeight.w600,
+    height: 1.5,
+  );
+
   static const TextStyle body1_700 = TextStyle(
     fontFamily: "Pretendard",
     fontSize: 16,
@@ -57,11 +64,11 @@ class DelliTextStyles {
     height: 1.38,
   );
 
-  static const TextStyle header2 = TextStyle(
+  static const TextStyle body4_500 = TextStyle(
     fontFamily: "Pretendard",
-    fontSize: 22,
-    fontWeight: FontWeight.w600,
-    height: 1.45,
+    fontSize: 12,
+    fontWeight: FontWeight.w500,
+    height: 1.33,
   );
 
   static const TextStyle button = TextStyle(
@@ -78,13 +85,31 @@ class DelliTextStyles {
     height: 1.43,
   );
 
+  static const TextStyle header2 = TextStyle(
+    fontFamily: "Pretendard",
+    fontSize: 22,
+    fontWeight: FontWeight.w600,
+    height: 1.45,
+  );
+
   static const TextStyle header3 = TextStyle(
     fontFamily: "Pretendard",
     fontSize: 20,
     fontWeight: FontWeight.w600,
     height: 1.4,
   );
-
+  static const TextStyle header4 = TextStyle(
+    fontFamily: "Pretendard",
+    fontSize: 18,
+    fontWeight: FontWeight.w500,
+    height: 1.33,
+  );
+  static const TextStyle header4_600 = TextStyle(
+    fontFamily: "Pretendard",
+    fontSize: 18,
+    fontWeight: FontWeight.w600,
+    height: 1.33,
+  );
   static const TextStyle header5 = TextStyle(
     fontFamily: "Pretendard",
     fontSize: 16,
